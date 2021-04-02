628e86291c8f9976b525deefa7952978843f85fd
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2021-04-02 15:12:45 -0300

    feat: include pypi setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyiseven',
+    version='0.3.1',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests>=2.14.0',
+        'retry>=0.9.2',
+    ],
+)

ef76c3f9a8f8733b34d343bc7042495fc8d19eeb
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2021-07-11 11:02:50 -0300

    fix: py2 code now running

--- is_even/_py2_is_even.py
+++ is_even/_py2_is_even.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 
 import requests
-from functools import lru_cache
-from retry import retry
-from typing import Union
 
 
-@lru_cache(maxsize=None)
-@retry(ConnectionError, tries=3, delay=2)
 def is_even(number):
     n = int(number)
-    r = requests.get(f"https://api.isevenapi.xyz/api/iseven/{n}/")
+    r = requests.get("https://api.isevenapi.xyz/api/iseven/" + str(n) + "/")
 
-    if r.status_code == requests.codes.ok:
+    if r.ok:
         return r.json()["iseven"]
     else:
         raise Exception(r.json()["error"])

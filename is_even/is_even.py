b73661e68b54d9c173104106eb00084742fb9369
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2021-07-21 20:28:04 -0300

    fix: change release from python 3.7 to 3.8

--- is_even/is_even.py
+++ is_even/is_even.py
@@ -7,8 +7,8 @@ __all__ = (
 
 if version_info >= (3, 10):
     from ._py3_10_is_even import is_even, is_odd
-elif version_info >= (3, 7):
-    from ._py3_7_is_even import is_even, is_odd
+elif version_info >= (3, 8):
+    from ._py3_8_is_even import is_even, is_odd
 elif version_info >= (3, 0):
     from ._py3_is_even import is_even, is_odd
 else:

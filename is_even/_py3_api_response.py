20ebc3a9f237d009f1a011440cb6ec14a04839c3
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2021-08-06 19:17:55 -0300

    fix: change var name as python convetion

--- is_even/_py3_api_response.py
+++ is_even/_py3_api_response.py
@@ -1,8 +1,8 @@
 class ISEVEN_APIresponse(int):
-    def __new__(self, value: int, ad: str) -> int:
-        self.ad = ad
-        self.value = value
-        return int.__new__(self, bool(value))
+    def __new__(cls, value: int, ad: str) -> int:
+        cls.ad = ad
+        cls.value = value
+        return int.__new__(cls, bool(value))
 
     def __repr__(self) -> str:
         return str(bool(self.value))

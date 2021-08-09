4f850e1f075b027b43fd71694886365e7f985257
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2021-08-09 20:07:19 -0300

    fix: remove redundant exception

--- is_even/_py3_8_is_even.py
+++ is_even/_py3_8_is_even.py
@@ -4,7 +4,7 @@ import requests
 from functools import lru_cache
 from retry import retry
 from typing import TYPE_CHECKING, Union
-from requests.exceptions import RequestException, ConnectTimeout
+from requests.exceptions import RequestException
 
 from ._typings import Success, Error
 from ._py3_api_response import IsEven
@@ -29,7 +29,7 @@ def is_even(number: Union[str, int]) -> TypeGuard[int]:
             raise Exception(json["error"])
         else:
             return IsEven(json["iseven"], json["ad"])
-    except (RequestException, ConnectTimeout):
+    except RequestException:
         return IsEven(_is_even(n), "Python Software Foundation rocks!")
 
 
